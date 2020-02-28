b5b7ef801ce703ea605725a0b7d80937e21cee6a
johnj974 <jjdug@hotmail.com>
2020-02-28 09:24:14 +0000

    added delete button to events page

--- app.py
+++ app.py
@@ -16,32 +16,13 @@ app.config["MONGO_URI"] = MONGO_URI
 
 mongo = PyMongo(app)
 
+
+
 @app.route("/")                                                                                     # home page
 @app.route("/home")
 def index():
     return render_template("home.html", check=mongo.db.checklist.find(),                            # check is assigned variable/ checklist is mongodb doc heading
                                         types=mongo.db.types.find())                                # events is assigned variable to mongodb heading
-                                       
-                                        
-
-@app.route("/add_list")                                                                             # page for checklist addition
-def add_list():
-    return render_template("list.html", disciplines=mongo.db.disciplines.find(),
-                                        events=mongo.db.events.find(),
-                                        types=mongo.db.types.find())
-
-
-@app.route("/blog_list")                                                                            # page for blog
-def blog_list():
-    return render_template("blog.html", entries=mongo.db.blog.find())
-
-
-@app.route("/insert_blog", methods=["POST"])                                                        # function to generate new blog
-def insert_blog():
-    blog = mongo.db.blog 
-    blog.insert_one(request.form.to_dict())
-    return redirect(url_for("blog_list"))
-
 
 
 @app.route("/insert_list", methods=["POST"])                                                        # function to generate new checklist to home page
@@ -49,19 +30,13 @@ def insert_list():
     checklist = mongo.db.checklist
     checklist.insert_one(request.form.to_dict())
     return redirect(url_for("index"))
-
-
-@app.route("/event_list")                                                                            # page for events list
-def event_list():
-    return render_template("events.html", events=mongo.db.events.find())
-
-
-
-@app.route("/insert_event", methods=["POST"])                                                        # function to generate new event list to event page
-def insert_event():
-    events = mongo.db.events
-    events.insert_one(request.form.to_dict())
-    return redirect(url_for("event_list"))
+                                      
+                                        
+@app.route("/add_list")                                                                             # page for checklist addition
+def add_list():
+    return render_template("list.html", disciplines=mongo.db.disciplines.find(),
+                                        events=mongo.db.events.find(),
+                                        types=mongo.db.types.find())
 
 
 @app.route("/edit_checklist/<checklist_id>")                                                        # function to edit checklist
@@ -72,8 +47,7 @@ def edit_checklist(checklist_id):
                            disciplines=all_disciplines)
 
 
-
-@app.route('/update_list/<checklist_id>', methods=["POST"])                                        
+@app.route('/update_list/<checklist_id>', methods=["POST"])                                         # update path to update checklist 
 def update_list(checklist_id):
     checklist = mongo.db.checklist
     checklist.update({'_id': ObjectId(checklist_id)},
@@ -90,10 +64,45 @@ def update_list(checklist_id):
     return redirect(url_for('index'))
 
 
-@app.route("/delete_checklist/<checklist_id>")
+@app.route("/delete_checklist/<checklist_id>")                                                      # function to delete checklist
 def delete_checklist(checklist_id):
     mongo.db.checklist.remove({"_id": ObjectId(checklist_id)})
     return redirect(url_for("index"))
+
+
+
+@app.route("/blog_list")                                                                            # page for blog
+def blog_list():
+    return render_template("blog.html", entries=mongo.db.blog.find())
+
+
+@app.route("/insert_blog", methods=["POST"])                                                        # function to generate new blog
+def insert_blog():
+    blog = mongo.db.blog 
+    blog.insert_one(request.form.to_dict())
+    return redirect(url_for("blog_list"))
+
+
+
+@app.route("/event_list")                                                                            # page for events list
+def event_list():
+    return render_template("events.html", events=mongo.db.events.find())
+
+
+@app.route("/insert_event", methods=["POST"])                                                        # function to generate new event list to event page
+def insert_event():
+    events = mongo.db.events
+    events.insert_one(request.form.to_dict())
+    return redirect(url_for("event_list"))
+
+
+@app.route("/delete_events/<events_id>")                                                             # function to delete event list
+def delete_events(events_id):
+    mongo.db.events.remove({"_id": ObjectId(events_id)})
+    return redirect(url_for("event_list"))
+
+
+
 
 
 
